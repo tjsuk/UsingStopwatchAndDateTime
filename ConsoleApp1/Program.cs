87f402806f31253a2a3a4e448d4b5dee807c22cd
Trevor Smith <156339246+tjsuk@users.noreply.github.com>
2024-03-27 21:06:26 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime start = DateTime.Now;
+        LoopString();
+        DateTime end = DateTime.Now;
+        Console.WriteLine($"DateTime LoopString:          {end - start}");
+
+        start = DateTime.Now;
+        LoopStringBuilder();
+        end = DateTime.Now;
+        Console.WriteLine($"DateTime LoopStringBuilder:   {end - start}");
+
+        Console.WriteLine("==============================================");
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        LoopString();
+        sw.Stop();
+        Console.WriteLine($"Stopwatch LoopString:         {sw.Elapsed}");
+
+        sw.Restart();
+        LoopStringBuilder();
+        sw.Stop();
+        Console.WriteLine($"Stopwatch LoopStringBuilder:  {sw.Elapsed}");
+
+    }
+
+    public static void LoopString()
+    {
+        var str = "Hello World!";
+        for (int i = 0; i < 1000; i++)
+        {
+            str += i;
+        }
+    }
+
+    public static void LoopStringBuilder()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Hello World!");
+        for (int i = 0; i < 1000; i++)
+        {
+            sb.Append(i);
+        }
+    }
+
+}
